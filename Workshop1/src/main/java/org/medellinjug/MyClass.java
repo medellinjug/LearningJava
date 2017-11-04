069d87f386d5e1263c4d3c8f7a9e378485e0d2a9
Hilmerc <hilac@msn.com>
2017-11-04 11:34:41 -0500

    Adding project with Maven Structure

---
+++ Workshop1/src/main/java/org/medellinjug/MyClass.java
@@ -0,0 +1,8 @@
+package org.medellinjug;
+
+public class MyClass{
+
+    public  static void main(String... args){
+        System.out.println("Hi world");
+    }
+}
---
+++ Workshop1/src/test/java/org/workshop1/MyClassTest.java
@@ -0,0 +1,14 @@
+package org.workshop1;
+
+import org.junit.Test;
+import org.medellinjug.MyClass;
+
+public class MyClassTest {
+
+    @Test
+    public void myTest(){
+        MyClass.main();
+
+
+    }
+}
